6cfdd8a8eed86fec7c5d3b7f41e8078b5ac16a8d
cj_gu <cj_gu@DESKTOP-T7MOQ3L>
2021-07-21 23:45:35 -0500

    Esta es la primera vercion de mi proyecto para POO en el cual he practicado el manejo de metodos

---
+++ src/main/java/ec/edu/intsuperior/modelo/CLaseA.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.modelo;
+
+/**
+ *
+ * @author cj_gu
+ */
+public class CLaseA {
+    public static void Caminar(){
+        System.out.println("hola yo soy el metodo para caminar");{
+    }
+    }    
+    public static String retornarDatos(){
+        
+      String x;
+      x="hola";
+      return x;
+        
+    }
+    public void vasilar(){
+        System.out.println("Hoy no estudio POO por q me voy a vacilar");
+       
+    }
+    //metodo de instancia de retorno
+
+    /**
+     *
+     * @return
+     */
+    public boolean estudiar(){
+       
+        return 5==6;
+                
+    }
+}
---
+++ src/main/java/ec/edu/intsuperior/vista/Aplication.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.vista;
+
+
+import ec.edu.intsuperior.modelo.ClaseA;
+
+
+
+/**
+ *
+ * @author cj_gu
+ */
+public class Aplication{
+    
+    public static void main(String[]argo){
+        
+          ClaseA obj=new ClaseA ();
+          System.out.println (obj.estudiar ());
+    }
+    
+
+  
+
+    private static class main extends ClaseA {
+
+        public main() {
+        }
+    }
+    
+    
+}
+    
+
